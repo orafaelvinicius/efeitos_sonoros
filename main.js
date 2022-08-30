a98fd63b0a4ebbd976e0671d8e0b5ea74f217c7f
Rafael <rafael@beanalytic.com.br>
2022-08-30 17:30:15 -0300

    Criando lista de elementos

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
+// Funcao para reproduzir som
 function tocaSomPom () {
     
     document.querySelector('#som_tecla_pom').play();
     return 'tocou pom'
 }
-document.querySelector('.tecla_pom').onclick=tocaSomPom;
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-    return 'tocou clap'
-}
-document.querySelector('.tecla_clap').onclick=tocaSomClap;
+// Criando lista de itens a partir da classe do elemento button
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// Acessando todos os elementos da lista
+listaDeTeclas[0].onclick = tocaSomPom;
+
 
 
 
