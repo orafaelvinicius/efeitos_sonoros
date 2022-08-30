b7b103805e4cbb431499da34d55f136f6537a71c
Rafael <rafael@beanalytic.com.br>
2022-08-30 18:10:54 -0300

    melhorando o codigo com a estrutura for

--- main.js
+++ main.js
@@ -8,25 +8,39 @@ function tocaSom (idElementoAudio) {
 // Criando lista de itens a partir da classe do elemento button
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-// Contador de repeticoes
-let contador = 0;
+// // Contador de repeticoes
+// let contador = 0;
 
-// Estrutura de repeticoes while
-while (contador < listaDeTeclas.length) {
+// // Estrutura de repeticoes while
+// while (contador < listaDeTeclas.length) {
+
+//     const tecla = listaDeTeclas[contador]
+    
+//     // capurando texto dinamico da tag button
+//     const instrumento = tecla.classList[1];
+    
+//     // template string
+//     const idAudio = `#som_${instrumento}`;
+
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     }
+
+//     contador += 1 
+
+// }
+
+// Estrutura de repeticoes for
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador]
-    
-    // capurando texto dinamico da tag button
-    const instrumento = tecla.classList[1];
-    
-    // template string
-    const idAudio = `#som_${instrumento}`;
+    const instrumento = tecla.classList[1];  // capurando texto dinamico da tag button
+    const idAudio = `#som_${instrumento}`; // template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador += 1 
-
 }
+
 
