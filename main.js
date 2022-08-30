7a78478481683ee8013f7273adc54702595684fe
Rafael <rafael@beanalytic.com.br>
2022-08-30 19:00:50 -0300

    Eventos, lógica e estruturas de repeticao

--- main.js
+++ main.js
@@ -1,17 +1,53 @@
 // Funcao para reproduzir sons
-function tocaSom (idElementoAudio) {
+function tocaSom (seletorAudio) {
     
-    document.querySelector(idElementoAudio).play();
-    return 'tocou som'
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play()
+    }
+    else {
+        alert('Elemento ou seletor inválido')
+        console.log('Elemento ou seletor inválido')
+    }
 }
 
 // Criando lista de itens a partir da classe do elemento button
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+// Estrutura de repeticoes for (otimizando o while)
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];  // capurando texto dinamico da tag button
+    const idAudio = `#som_${instrumento}`; // template string
+    
+    // funcao para click na tecla
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    // funcao para tecla apertada
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter' || evento.code === 'NumpadEnter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    // funcao para tecla solta
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+
+}
+
+
+// // Estrutura de repeticoes while
+
 // // Contador de repeticoes
 // let contador = 0;
 
-// // Estrutura de repeticoes while
 // while (contador < listaDeTeclas.length) {
 
 //     const tecla = listaDeTeclas[contador]
@@ -29,18 +65,3 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 //     contador += 1 
 
 // }
-
-// Estrutura de repeticoes for
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
-
-    const tecla = listaDeTeclas[contador]
-    const instrumento = tecla.classList[1];  // capurando texto dinamico da tag button
-    const idAudio = `#som_${instrumento}`; // template string
-
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    }
-
-}
-
-
